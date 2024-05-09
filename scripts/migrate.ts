bc028453fbdb79c5f24870b2e9b8e03d8cdbc628
Grey Boolean <greyboolean@gmail.com>
2024-05-09 11:51:34 +0530

    change database name in migrate file

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -3,6 +3,6 @@ import { execSync } from 'child_process';
 const tenants = ['tenant1', 'tenant2', 'tenant3']; // replace with your actual tenants
 
 for (const tenant of tenants) {
-  process.env.DATABASE_URL = `postgresql://user:password@localhost:5432/mydatabase?schema=${tenant}`;
+  process.env.DATABASE_URL = `postgresql://user:password@localhost:5432/nest-multi-tenant?schema=${tenant}`;
   execSync('npx prisma migrate dev', { stdio: 'inherit' });
 }
